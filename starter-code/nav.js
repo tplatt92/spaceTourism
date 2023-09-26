d51dc55412a3d6934b744984b892cf15b6a6efe6
jjw9494 <joejameswalsh94@gmail.com>
2023-09-26 21:28:39 +0100

    more pain

---
+++ starter-code/nav.js
@@ -0,0 +1,16 @@
+const primaryNav = document.querySelector(".nav__links");
+const navToggle = document.querySelector(".mobile__nav__toggle");
+
+navToggle.addEventListener("click", () => {
+	const visibility = primaryNav.getAttribute("data-visible");
+
+	if (visibility === "false") {
+		primaryNav.setAttribute("data-visible", true);
+		navToggle.setAttribute("aria-expanded", true);
+	} else if (visibility === "true") {
+		primaryNav.setAttribute("data-visible", false);
+		navToggle.setAttribute("aria-expanded", false);
+	}
+
+	console.log(visibility);
+});
