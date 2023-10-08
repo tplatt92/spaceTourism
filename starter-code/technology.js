28c6678e13294bc5815278f0331f8505d30d0730
Tom Platt <thomasjplatt0107@gmail.com>
2023-10-08 10:15:55 +0100

    links and read me markup started

---
+++ starter-code/technology.js
@@ -0,0 +1,64 @@
+console.log("ai");
+
+// Scroll Effects
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+let mm = gsap.matchMedia();
+
+// Scroll effect on crew panels
+
+// Desktop & Tablet
+mm.add("(min-width: 34.98em)", () => {
+  gsap.set("main", { position: "absolute" });
+
+  gsap.to("main > :nth-child(3), main > :nth-child(2))", {
+    xPercent: 100,
+    stagger: -0.5,
+    scrollTrigger: {
+      trigger: document.body,
+      markers: false,
+      start: "top top",
+      end: "+=2000",
+      scrub: 1,
+      pin: true,
+      pinSpacing: true,
+      anticipatePin: 1,
+    },
+  });
+});
+
+// Paralax effect on Text
+
+mm.add("(min-width: 62em)", () => {
+  gsap.to(".technology__info__wrapper", {
+    xPercent: 10,
+    stagger: -0.5,
+
+    scrollTrigger: {
+      trigger: document.body,
+      markers: false,
+      start: "top top",
+      end: "+=2000",
+      scrub: 1,
+      pin: true,
+      pinSpacing: true,
+      anticipatePin: 1,
+    },
+  });
+
+  gsap.to(".technology__photo", {
+    xPercent: 5,
+    stagger: -0.5,
+
+    scrollTrigger: {
+      trigger: document.body,
+      markers: false,
+      start: "top top",
+      end: "+=2000",
+      scrub: 1,
+      pin: true,
+      pinSpacing: true,
+      anticipatePin: 1,
+    },
+  });
+});
