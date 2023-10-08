4a7e1841f10f34037eade0d998cdcccfdb339ea6
jjw9494 <joejameswalsh94@gmail.com>
2023-10-08 22:37:48 +0100

    fixed alignment bugs

---
+++ starter-code/technology2.js
@@ -0,0 +1,61 @@
+// Scroll Effects
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+let mm = gsap.matchMedia();
+
+// Scroll effect on destination panels
+mm.add("(min-width: 35em)", () => {
+	gsap.set(".technology_container", { position: "absolute" });
+
+	gsap.to(".home__wrapper > :nth-child(4), .home__wrapper > :nth-child(3)", {
+		xPercent: 100,
+		stagger: -0.5,
+		scrollTrigger: {
+			trigger: document.body,
+			markers: false,
+			start: "top top",
+			end: "+=2000",
+			scrub: 1,
+			pin: true,
+			pinSpacing: true,
+		},
+	});
+});
+
+// Paralax effect on Text
+
+mm.add("(min-width: 64em)", () => {
+	// gsap.set(".desktop__flex_text", { position: "absolute" });
+
+	gsap.to(".home__title", {
+		xPercent: 5,
+		stagger: -0.5,
+
+		scrollTrigger: {
+			trigger: document.body,
+			markers: false,
+			start: "top top",
+			end: "+=2000",
+			scrub: 1,
+			pin: true,
+			pinSpacing: true,
+			anticipatePin: 1,
+		},
+	});
+
+	gsap.to(".home__text", {
+		xPercent: 10,
+		stagger: -0.5,
+
+		scrollTrigger: {
+			trigger: document.body,
+			markers: false,
+			start: "top top",
+			end: "+=2000",
+			scrub: 1,
+			pin: true,
+			pinSpacing: true,
+			anticipatePin: 1,
+		},
+	});
+});
