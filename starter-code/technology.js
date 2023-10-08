7e6375b54d5ac9fbe0c1f6c11e511d51b5f85c9b
jjw9494 <joejameswalsh94@gmail.com>
2023-10-08 13:19:27 +0100

    Tablet bug troubleshooting

--- starter-code/technology.js
+++ starter-code/technology.js
@@ -8,57 +8,57 @@ let mm = gsap.matchMedia();
 // Scroll effect on crew panels
 
 // Desktop & Tablet
-mm.add("(min-width: 34.98em)", () => {
-  gsap.set("main", { position: "absolute" });
+mm.add("(min-width: 35em)", () => {
+	gsap.set("main", { position: "absolute" });
 
-  gsap.to("main > :nth-child(3), main > :nth-child(2))", {
-    xPercent: 100,
-    stagger: -0.5,
-    scrollTrigger: {
-      trigger: document.body,
-      markers: false,
-      start: "top top",
-      end: "+=2000",
-      scrub: 1,
-      pin: true,
-      pinSpacing: true,
-      anticipatePin: 1,
-    },
-  });
+	gsap.to("main > :nth-child(4), main > :nth-child(3)", {
+		xPercent: 100,
+		stagger: -0.5,
+		scrollTrigger: {
+			trigger: document.body,
+			markers: false,
+			start: "top top",
+			end: "+=2000",
+			scrub: 1,
+			pin: true,
+			pinSpacing: true,
+			anticipatePin: 1,
+		},
+	});
 });
 
 // Paralax effect on Text
 
 mm.add("(min-width: 62em)", () => {
-  gsap.to(".technology__info__wrapper", {
-    xPercent: 10,
-    stagger: -0.5,
+	gsap.to(".technology__info__wrapper", {
+		xPercent: 10,
+		stagger: -0.5,
 
-    scrollTrigger: {
-      trigger: document.body,
-      markers: false,
-      start: "top top",
-      end: "+=2000",
-      scrub: 1,
-      pin: true,
-      pinSpacing: true,
-      anticipatePin: 1,
-    },
-  });
+		scrollTrigger: {
+			trigger: document.body,
+			markers: false,
+			start: "top top",
+			end: "+=2000",
+			scrub: 1,
+			pin: true,
+			pinSpacing: true,
+			anticipatePin: 1,
+		},
+	});
 
-  gsap.to(".technology__photo", {
-    xPercent: 5,
-    stagger: -0.5,
+	gsap.to(".technology__photo", {
+		xPercent: 5,
+		stagger: -0.5,
 
-    scrollTrigger: {
-      trigger: document.body,
-      markers: false,
-      start: "top top",
-      end: "+=2000",
-      scrub: 1,
-      pin: true,
-      pinSpacing: true,
-      anticipatePin: 1,
-    },
-  });
+		scrollTrigger: {
+			trigger: document.body,
+			markers: false,
+			start: "top top",
+			end: "+=2000",
+			scrub: 1,
+			pin: true,
+			pinSpacing: true,
+			anticipatePin: 1,
+		},
+	});
 });
